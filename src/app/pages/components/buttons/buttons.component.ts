fe1b25e3e1dfd42af6c4fed311408dafab4fa481
superdev <superdevp@gmail.com>
2025-03-10 18:10:54 +0200

    f structure

---
+++ src/app/pages/components/buttons/buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../shared/components/button/button.component";
+
+@Component({
+  selector: 'app-buttons',
+  imports: [ButtonComponent],
+  templateUrl: './buttons.component.html',
+  styleUrl: './buttons.component.scss'
+})
+export class ButtonsComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../../shared/components/button/button.component";
+import { ButtonsComponent } from "../components/buttons/buttons.component";
 
 @Component({
   selector: 'app-main',
-  imports: [ButtonComponent],
+  imports: [ButtonsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
